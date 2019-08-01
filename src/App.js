72c93c3eccc37cf5c967a49157fa99e3abd61156
mhabzda <marcin.habzda1@gmail.com>
2019-08-01 09:15:58 +0200

    Add state manipulation

--- src/App.js
+++ src/App.js
@@ -8,18 +8,32 @@ class App extends Component {
       {name: 'Max', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Stephanie', age: 26}
-    ]
+    ],
+    otherState: 'other value'
+  };
+
+  switchNameHandler = () => {
+    // console.log('Was clicked!');
+    // DON'T this.state.persons[0].name = 'Maximilian'
+    this.setState({
+      persons: [
+        {name: 'Maximilian', age: 28},
+        {name: 'Manu', age: 29},
+        {name: 'Stephanie', age: 27}
+      ]
+    });
   };
 
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button>Switch name</button>
+        <button onClick={this.switchNameHandler}>Switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
           My hobbies: Racing
         </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
