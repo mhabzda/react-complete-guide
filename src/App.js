bff47c2f0963c83bc805c72f487d064dd3148033
mhabzda <marcin.habzda1@gmail.com>
2019-08-05 08:45:09 +0200

    Style Person component

--- src/App.js
+++ src/App.js
@@ -35,10 +35,20 @@ const app = props => {
     });
   };
 
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  };
+
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
-      <button onClick={() => switchNameHandler('Maximilian')}>Switch name</button>
+      <button
+        style={style}
+        onClick={() => switchNameHandler('Maximilian')}>Switch name</button>
       <Person
         name={personsState.persons[0].name}
         age={personsState.persons[0].age}/>
