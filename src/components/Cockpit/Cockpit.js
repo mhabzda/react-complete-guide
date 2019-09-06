9a082f8437e9bb865dabdf884af45a6f1a03e8dc
mhabzda <marcin.habzda1@gmail.com>
2019-09-06 09:28:23 +0200

    Add app title

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -3,7 +3,7 @@ import classes from "./Cockpit.css";
 
 const cockpit = (props) => {
   let buttonClass = '';
-  if(props.showPersons) {
+  if (props.showPersons) {
     buttonClass = classes.Red;
   }
 
@@ -17,7 +17,7 @@ const cockpit = (props) => {
 
   return (
     <div className={classes.Cockpit}>
-      <h1>Hi, I'm a React App</h1>
+      <h1>{props.title}</h1>
       <p className={assignedClasses.join(' ')}>This is really working</p>
       <button
         className={buttonClass}
