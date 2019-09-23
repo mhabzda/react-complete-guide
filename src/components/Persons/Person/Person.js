6ec69b775b14077b1193fd7705182bd3c7723b89
mhabzda <marcin.habzda1@gmail.com>
2019-09-23 09:10:12 +0200

    Use higher order components and propTypes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,27 @@
-import React, {Component} from "react";
-import classes from './Person.css';
+import React, {Component, Fragment} from "react";
+import PropTypes from 'prop-types'
+
+import withClass from "../../../hoc/withClass";
+import classes from './Person.css'
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
     return (
-      <div className={classes.Person}>
+      <Fragment>
         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-        <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name}/>
-      </div>
+        <p key="i2">{this.props.children}</p>
+        <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
+      </Fragment>
     )
   };
 }
 
-export default Person;
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
+
+export default withClass(Person, classes.Person);
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import classes from './App.css';
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
+import withClass from '../hoc/withClass'
+import Aux from '../hoc/Aux'
 
 class App extends Component {
   constructor(props) {
@@ -82,7 +84,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <Aux>
         <button onClick={() => {
           this.setState({showCockpit: false})
         }}>Remove Cockpit
@@ -93,10 +95,10 @@ class App extends Component {
           personsLength={this.state.persons.length}
           clicked={this.togglePersonsHandler}/> : null}
         {persons}
-      </div>
+      </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
 
